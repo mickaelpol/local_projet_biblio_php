8f87458cb6f84d74c6c6e5599e41076bceb90348
Léo Subias <leo.subias@gmail.com>
2017-09-24 14:01:20 +0200

    ajout connextionAdmin et scipt js app.js

---
+++ page/connectionAdmin.php
@@ -0,0 +1,13 @@
+
+<form id='form'>
+   <!-- <label for="pseudo">Pseudonyme</label> -->
+   <div class="form-group" id="test">
+     <label class="control-label" for="pseudo">Pseudonyme</label>
+     <input type="text" class="champ form-control" id="pseudo">
+   </div>
+   <div class="form-group" id="test2">
+   <label class="control-label" for="mdp">mot de passe</label>
+     <input type="password" class="champ2 form-control" id="mdp">
+   </div>
+   <input class="btn btn-info" type="submit" id="envoi" value="Envoyer" />
+ </form>
--- page/js/app.js
+++ page/js/app.js
@@ -1,5 +1,34 @@
-$(function(){
+$(document).ready(function(){
 
-	
+  var pseudo = $('#pseudo'),
+  mdp = $('#mdp'),
+  test = $("#test"),
+  test2 = $('#test2'),
+  envoi = $("#envoi");
 
-})
+
+  $("#form").submit(function(e){
+
+ // PSEUDO
+
+// validation ou erreur de saisie a modifier en vonction du PHP
+
+    if (pseudo.val().length <5 || champ.val().length >25) {
+      test.addClass("has-error");
+      e.preventDefault();
+
+    }
+
+//  MOT DE PASSE
+
+// validation ou erreur de saisie a modifier en vonction du PHP
+
+    if (mdp.val().length <5 || champ2.val().length >25) {
+      test2.addClass("has-error");
+      e.preventDefault();
+
+    }
+
+
+  })
+});
